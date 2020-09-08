d8893d6e79e1945c7ef0e0851314d7266e27ad5b
Andrew Davisson <akdavisson4@gmail.com>
2020-09-08 11:23:34 -0600

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I'm good enough, I'm smart enough, and gosh darn it, people like me!
+      </p>
+    </div>
+  )
+}
+
+export default About
