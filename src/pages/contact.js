4979d0f869f9fbb944d9c6bed0fb0704dba2714a
Andrew Davisson <akdavisson4@gmail.com>
2020-09-08 11:26:09 -0600

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>I'd lvoe to talk! Email me at the address below</h1>
+      <p>
+        <a href='mailto:akdavisson@gamil.com'>akdavisson@gmail.com</a>
+      </p>
+    </div>
+  )
+}
+
+export default Contact
